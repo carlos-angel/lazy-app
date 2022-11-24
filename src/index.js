671f8474ee9606cd685e2093e4f3036b80178c2c
carlos alberto angel angel <c.a.a.a.503@gmail.com>
2022-11-24 10:10:24 -0600

    feat: agregar una imagen a la lista

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const BASE_URL = 'https://randomfox.ca';
 
-console.log('Happy hacking :)')
+const mountNode = document.getElementById('images');
+
+const addButton = document.querySelector('button');
+function addImagen() {
+  const src = `${BASE_URL}/images/${random()}.jpg`;
+  const newImage = createImageNode(src);
+  mountNode.append(newImage);
+}
+
+addButton.addEventListener('click', addImagen);
+
+function random() {
+  const maximum = 122;
+  const minimum = 1;
+  return Math.floor(Math.random() * (maximum - minimum)) + minimum;
+}
+
+function createImageNode(src) {
+  const container = document.createElement('div');
+  container.className = 'p-4';
+
+  const image = document.createElement('img');
+  image.className = 'mx-auto';
+  image.width = 320;
+  image.src = src;
+
+  container.appendChild(image);
+
+  return container;
+}
